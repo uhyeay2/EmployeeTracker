d925f5a2b1d5d652b58acc9f4acb2c9482514daf
Daniel Aguirre <uhyeay24@gmail.com>
2023-02-11 19:00:10 -0600

    updated global usings

---
+++ EmployeeTracker.Mediator.Tests/Usings.cs
@@ -0,0 +1,5 @@
+global using Xunit;
+global using Shouldly;
+
+global using EmployeeTracker.Mediator.Constants;
+global using EmployeeTracker.Mediator.Tests.TestHelpers;
---
+++ EmployeeTracker.Mediator/Usings.cs
@@ -0,0 +1,10 @@
+﻿
+global using EmployeeTracker.Data.Interfaces;
+global using EmployeeTracker.Domain.Extensions;
+global using EmployeeTracker.Domain.Models;
+global using EmployeeTracker.Mediator.Abstractions.BaseHandlers;
+global using EmployeeTracker.Mediator.Abstractions.BaseRequests;
+global using EmployeeTracker.Mediator.Abstractions.BaseResponses;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("EmployeeTracker.Mediator.Tests")]
