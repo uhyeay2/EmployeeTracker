c8cce6be446480d1a113cadf972b62eba8ecb1f3
Daniel Aguirre <uhyeay24@gmail.com>
2023-02-11 18:58:13 -0600

    Created logic for ValidationBehavior

---
+++ EmployeeTracker.Mediator/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeTracker.Domain.Exceptions;
+using EmployeeTracker.Domain.Interfaces;
+using MediatR;
+
+namespace EmployeeTracker.Mediator.Behaviors
+{
+    internal sealed class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : class, IRequest<TResponse>
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if(request is IValidatable validatable && !validatable.IsValid(out var validationFailures))
+            {
+                throw new ValidationFailedException(validationFailures);
+            }
+
+            return await next();
+        }
+    }
+}
