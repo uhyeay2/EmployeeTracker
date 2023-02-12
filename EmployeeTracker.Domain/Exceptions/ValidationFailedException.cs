7f3e20d3373abde8b1aa7815274205e15e29fdd4
Daniel Aguirre <uhyeay24@gmail.com>
2023-02-11 18:57:26 -0600

    Created ValidationFailedException

---
+++ EmployeeTracker.Domain/Exceptions/ValidationFailedException.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeTracker.Domain.Models;
+
+namespace EmployeeTracker.Domain.Exceptions
+{
+    public class ValidationFailedException : Exception
+    {
+        public ValidationFailedException(IEnumerable<ValidationFailure> validationFailures)
+        {
+            ValidationFailures = validationFailures;
+        }
+
+        public IEnumerable<ValidationFailure> ValidationFailures { get; set; }
+    }
+}
