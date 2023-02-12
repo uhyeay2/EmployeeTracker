35303f4536f4966c77b7ec06b4deac5d6821832b
Daniel Aguirre <uhyeay24@gmail.com>
2023-02-11 18:59:39 -0600

    Set up Mediator Layer Dependency Injection

---
+++ EmployeeTracker.Mediator/DependencyInjection/MediatorServices.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeTracker.Data.DependencyInjection;
+using EmployeeTracker.Domain.Interfaces;
+using EmployeeTracker.Mediator.Behaviors;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmployeeTracker.Mediator.DependencyInjection
+{
+    public static class MediatorServices
+    {
+        public static IServiceCollection InjectMediatorServices(this IServiceCollection services, IConfig databaseConfig)
+        {
+            services.InjectDataServices(databaseConfig);
+            
+            services.AddMediatR(typeof(MediatorServices).Assembly);
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            return services;
+        }
+    }
+}
