ff9b8026aa2f876c735b950ce0f414e7fe44e8f6
Daniel Aguirre <uhyeay24@gmail.com>
2023-02-12 15:14:39 -0600

    Implemented DepartmentCodeExistsHandler w/ Tests

---
+++ EmployeeTracker.Mediator.Tests/HandlerTests/DepartmentHandlerTests/DepartmentCodeExistsHandlerTests.cs
@@ -0,0 +1,30 @@
+﻿using EmployeeTracker.Data.DataRequestObjects.DepartmentRequests;
+using EmployeeTracker.Mediator.Handlers.DepartmentHandlers;
+
+namespace EmployeeTracker.Mediator.Tests.HandlerTests.DepartmentHandlerTests
+{
+    public class DepartmentCodeExistsHandlerTests : DataHandlerTest
+    {
+        public DepartmentCodeExistsHandlerTests() => _handler = new(MockDataAccess.Object);
+
+        private readonly DepartmentCodeExistsHandler _handler;
+
+        private static readonly DepartmentCodeExistsRequest _request = new("Code");
+
+        [Fact]
+        public async Task DepartmentCodeExists_Given_DepartmentExists_Should_ReturnTrue()
+        {
+            SetupMockFetch<DepartmentCodeExists, bool>(true);
+
+            (await _handler.Handle(_request, default)).Content.ShouldBe(true);
+        }
+
+        [Fact]
+        public async Task DepartmentCodeExists_Given_DepartmentDoesNotExist_Should_ReturnFalse()
+        {
+            SetupMockFetch<DepartmentCodeExists, bool>(false);
+
+            (await _handler.Handle(_request, default)).Content.ShouldBe(false);
+        }
+    }
+}
---
+++ EmployeeTracker.Mediator/Handlers/DepartmentHandlers/DepartmentCodeExistsHandler.cs
@@ -0,0 +1,21 @@
+﻿using EmployeeTracker.Data.DataRequestObjects.DepartmentRequests;
+
+namespace EmployeeTracker.Mediator.Handlers.DepartmentHandlers
+{
+    public class DepartmentCodeExistsRequest : BaseRequest<BaseResponse<bool>>
+    {
+        public DepartmentCodeExistsRequest(string code) => Code = code;
+
+        public string Code { get; set; }
+    }
+
+    internal class DepartmentCodeExistsHandler : DataHandler<DepartmentCodeExistsRequest, BaseResponse<bool>>
+    {
+        public DepartmentCodeExistsHandler(IDataAccess dataAccess) : base(dataAccess) { }
+
+        public override async Task<BaseResponse<bool>> Handle(DepartmentCodeExistsRequest request, CancellationToken cancellationToken)
+        {
+            return Response.Success(await DataAccess.FetchAsync<bool>(new DepartmentCodeExists(request.Code)));
+        }
+    }
+}
