287130785caff29ec6ef81654d6f937c7b74efd1
Daniel Aguirre <uhyeay24@gmail.com>
2023-02-12 14:52:00 -0600

    Implemented UpdateDepartmentHandler w/ Tests

---
+++ EmployeeTracker.Mediator.Tests/HandlerTests/DepartmentHandlerTests/UpdateDepartmentHandlerTests.cs
@@ -0,0 +1,31 @@
+﻿using EmployeeTracker.Data.DataRequestObjects.DepartmentRequests;
+using EmployeeTracker.Domain.Models;
+using EmployeeTracker.Mediator.Handlers.DepartmentHandlers;
+
+namespace EmployeeTracker.Mediator.Tests.HandlerTests.DepartmentHandlerTests
+{
+    public class UpdateDepartmentHandlerTests : DataHandlerTest
+    {
+        public UpdateDepartmentHandlerTests() => _handler = new(MockDataAccess.Object);
+
+        private readonly UpdateDepartmentHandler _handler;
+
+        private static readonly UpdateDepartmentRequest _request = new(new Department("Code", "Name"));
+
+        [Fact]
+        public async Task UpdateDepartment_Given_NoDepartmentUpdated_Should_ReturnNotFoundResponse()
+        {
+            SetupMockExecute<UpdateDepartment>(0);
+
+            (await _handler.Handle(_request, default)).StatusCode.ShouldBe(StatusCodes.NotFound);
+        }
+
+        [Fact]
+        public async Task UpdateDepartment_Given_DepartmentUpdated_Should_ReturnSuccessResponse()
+        {
+            SetupMockExecute<UpdateDepartment>(1);
+
+            (await _handler.Handle(_request, default)).StatusCode.ShouldBe(StatusCodes.Success);
+        }
+    }
+}
---
+++ EmployeeTracker.Mediator/Handlers/DepartmentHandlers/UpdateDepartmentHandler.cs
@@ -0,0 +1,30 @@
+﻿using EmployeeTracker.Data.DataRequestObjects.DepartmentRequests;
+
+namespace EmployeeTracker.Mediator.Handlers.DepartmentHandlers
+{
+    public class UpdateDepartmentRequest : BaseRequest<DataExecutionResponse>
+    {
+        public UpdateDepartmentRequest(Department department) => Department = department;
+
+        public Department Department { get; set; }
+
+    }
+
+    internal class UpdateDepartmentHandler : DataHandler<UpdateDepartmentRequest, DataExecutionResponse>
+    {
+        public UpdateDepartmentHandler(IDataAccess dataAccess) : base(dataAccess) { }
+
+        public override async Task<DataExecutionResponse> Handle(UpdateDepartmentRequest request, CancellationToken cancellationToken)
+        {
+            var rowsAffected = await DataAccess.ExecuteAsync(new UpdateDepartment(request.Department));
+
+            if (rowsAffected == 1)
+            {
+                return DataExecutionResponse.Success(rowsAffected);
+            }
+
+            return DataExecutionResponse.NotFound();
+
+        }
+    }
+}
