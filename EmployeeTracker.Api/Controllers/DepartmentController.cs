b01a4f0518cd4051a6495e5ab8418f28be4aa1ab
Daniel Aguirre <uhyeay24@gmail.com>
2023-02-12 18:24:47 -0600

    Added Remaining Department Endpoints

--- EmployeeTracker.Api/Controllers/DepartmentController.cs
+++ EmployeeTracker.Api/Controllers/DepartmentController.cs
@@ -12,13 +12,22 @@ namespace EmployeeTracker.Api.Controllers
 
         public DepartmentController(IMediator mediator) => _mediator = mediator;
 
-        [HttpGet("GetAllDepartments")]
-        public async Task<BaseResponse<IEnumerable<Department>>> GetAllDepartments() => await _mediator.Send(new GetAllDepartmentsRequest());
-
         [HttpGet("GetDepartmentByCode")]
         public async Task<BaseResponse<Department>> GetDepartmentByCode(string code) => await _mediator.Send(new GetDepartmentByCodeRequest(code));
 
+        [HttpGet("GetAllDepartments")]
+        public async Task<BaseResponse<IEnumerable<Department>>> GetAllDepartments() => await _mediator.Send(new GetAllDepartmentsRequest());
+
+        [HttpGet("DepartmentCodeExists")]
+        public async Task<BaseResponse<bool>> DepartmentCodeExists(string code) => await _mediator.Send(new DepartmentCodeExistsRequest(code));
+
         [HttpPost("InsertDepartment")]
         public async Task<DataExecutionResponse> GetDepartmentByCode(InsertDepartmentRequest request) => await _mediator.Send(request);
+
+        [HttpPost("UpdateDepartment")]
+        public async Task<DataExecutionResponse> UpdateDepartment(Department department) => await _mediator.Send(new UpdateDepartmentRequest(department?.Code!, department?.Name!));
+
+        [HttpDelete("DeleteDepartment")]
+        public async Task<DataExecutionResponse> DeleteDepartmentByCode(string code) => await _mediator.Send(new DeleteDepartmentRequest(code));
     }
 }
